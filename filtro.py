7ddc08e117f2dc81063e4fd600d4a3cd67c6d35d
quatroka <monteiro.leonardosantos@gmail.com>
2018-10-20 13:54:17 -0300

    more filters

--- filtro.py
+++ filtro.py
@@ -2,4 +2,5 @@ WORDS_NOT_FOR_JOKE = ['mina', 'mulher', 'loira', 'preto', 'negro', 'vtnc', 'pqp'
               'sexo', 'altista', 'cego', 'gordo', 'magro', 'prostituta', 'criança', 'presidencia', 'cantada',
               'cool', 'olla', 'cu', 'presidente', 'porno', 'pornô', 'prostata', 'heroína', 'bunda', 'xuxa', 'padre',
               'abusivo', 'bumbum', 'rita', 'moça', 'candidato', 'daciolo', 'bolos', 'ânus', 'prostituta', 'cuzcuz', 'maconh',
-              'Jhonny', 'Maoísmo', 'livrinho', 'anus', 'ânus', 'conha', 'nagazap']
+              'Jhonny', 'Maoísmo', 'livrinho', 'anus', 'ânus', 'conha', 'nagazap', 'cacofonia', 'presidenciável', 'tequila', 'cool',
+              'Roller']
